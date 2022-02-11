5a43ff66c9777b23a99910fd36d2c7b20d1605b7
Seamus McGill <seamusmcgill@gmail.com>
2022-02-11 13:52:20 -0500

    Add reservation form to listings

--- LightBnB_WebApp-master/public/javascript/components/property_listing.js
+++ LightBnB_WebApp-master/public/javascript/components/property_listing.js
@@ -18,14 +18,54 @@ $(() => {
             `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>` 
             : ``}
           <footer class="property-listing__footer">
+          
             <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
             <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+            
           </footer>
+          <br>
+          <form action="/api/reservations" method="post" id="reservation-form"id="reservation-form" class="reservation-form">
+              <h6>Make a Reservation</h6>
+
+              <input hidden type="text" name="property_id" value="${property.id}"></input>
+              <div class="reservation-form">
+                  <label for="reservation-form__start_date">
+                  <input type="date" name="start_date" id="reservation-form__start_date">
+              </div>
+
+              <div class="reservation-form">
+                  <label for="end_date">
+                  <input type="date" name="end_date" id="reservation-form__end_date">
+              </div>
+              
+              <div class="reservation-form">
+                  <button>Reserve Now</button>
+              </div>
+              
+            </form>
         </section>
       </article>
     `
   }
 
   window.propertyListing.createListing = createListing;
+
+  ("#reservation-form").on('submit', function(event) {
+    event.preventDefault();
+
+    views_manager.show('none');
+
+    const data = $(this).serialize();
+    submitReservation(data)
+      .then(() => {
+        views_manager.show('newProperty');
+      })
+      .catch((error) => {
+        console.error(error);
+        views_manager.show('listings');
+      });
+  });
+
+
 
 });
